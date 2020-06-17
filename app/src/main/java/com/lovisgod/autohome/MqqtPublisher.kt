10d5798c22f63e0d0dd4c1b020ece9f3de6509dd
lovisgod <olifedayo94@gmail.com>
2020-06-17 23:07:50 +0100

    feature: started mqtt connection

--- app/src/main/java/com/lovisgod/autohome/MqqtPublisher.kt
+++ app/src/main/java/com/lovisgod/autohome/MqqtPublisher.kt
@@ -1,6 +1,9 @@
 package com.lovisgod.autohome
 
+import android.util.Log
 import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.IMqttActionListener
+import org.eclipse.paho.client.mqttv3.IMqttToken
 import org.eclipse.paho.client.mqttv3.MqttException
 
 import org.eclipse.paho.client.mqttv3.MqttMessage
@@ -16,7 +19,21 @@ class MqttPublisher {
             encodedPayload = event.toByteArray(charset("UTF-8"))
             val message = MqttMessage(encodedPayload)
             message.isRetained = true
-            client.publish(topic, message)
+            client.publish(topic, message).actionCallback  = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    // We are connected
+                    Log.d("TAG", "publish successful")
+                }
+
+                override fun onFailure(
+                    asyncActionToken: IMqttToken,
+                    exception: Throwable
+                ) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    println(exception.localizedMessage)
+                    Log.d("TAG", "publish failed")
+                }
+            }
         } catch (e: UnsupportedEncodingException) {
             e.printStackTrace()
         } catch (e: MqttException) {
